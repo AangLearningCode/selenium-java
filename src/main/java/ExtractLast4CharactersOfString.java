a887912f8534aa7b1f9d1edc9288d47524ac5813
AangLearningCode <anhavinh14061988@gmail.com>
2025-03-30 15:52:41 +0700

    RemoveDuplicateWordsFromSentence

---
+++ src/main/java/ExtractLast4CharactersOfString.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class ExtractLast4CharactersOfString {
+}
---
+++ src/main/java/FindFirstNoneRepeatedCharacter.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFirstNoneRepeatedCharacter {
+    public static void main(String[] args) {
+        String str = "aabbccddeeff";
+//        System.out.println(firstNonRepeatedCharacter(str));
+    }
+
+    public static void firstNonRepeatedCharacter(String str) {
+        Map<Character,Integer> map = new HashMap<>();
+        int couter = 1;
+
+    }
+}
