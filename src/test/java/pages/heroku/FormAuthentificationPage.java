3de7c9caf53d7fcd56f8550f68a5fc33a7d6a0f1
Aang <anhavinh14061988@gmail.com>
2025-05-10 11:13:32 +0700

    Create page for FromAuthentification Page and FormAuthenticationTest -> pass

---
+++ src/test/java/pages/heroku/FormAuthentificationPage.java
@@ -0,0 +1,9 @@
+package pages.heroku;
+
+import static utils.Browser.*;
+
+public class FromAuthentificationPage {
+    public void open(){
+        visit("https://the-internet.herokuapp.com/login");
+    }
+}
